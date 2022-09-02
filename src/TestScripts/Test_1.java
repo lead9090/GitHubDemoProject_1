00fb94edff1ae34ad84294a7850ef90a13eb431a
sebas <sebas@192.168.0.102>
2022-09-02 15:09:11 +0100

    This is my first commit

---
+++ src/TestScripts/Test_1.java
@@ -0,0 +1,16 @@
+package TestScripts;
+
+public class Test_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				int a =10,b=10;
+				if(a==b) {
+				System.out.println("both are same");
+				}
+				else{
+				System.out.println("both are not same");
+	}
+
+	}
+}
